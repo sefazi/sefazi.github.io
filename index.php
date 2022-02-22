3d7691a3cfffede765a5f7f4760245e9bdd252d5
Kijaku <94213420+Kijaku@users.noreply.github.com>
2022-02-22 22:54:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 <html>
   <body>
     <?php
-        reaquire_once ('https://github.com/Kijaku/kijaku.github.io.git/app/init.php');
-     
-        $app = new App;
+        echo 'hallo';
     ?>
   </body>
 </html>
